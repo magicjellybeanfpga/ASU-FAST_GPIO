cd631b5c41fc93dd466cf64c8febe3e257396dc3
abustillos7 <55632844+abustillos7@users.noreply.github.com>
2022-01-31 22:40:53 -0600

    Added comments and Attempted Counter Connection

--- Fast_GPIO_Design.v
+++ Fast_GPIO_Design.v
@@ -1,4 +1,6 @@
 `timescale 10 ns / 1 ns
+
+// This module is a basic register used for individual GPIO_IN# and GPIO_OUT# in the diagram, each holding 32 bits
 module gpio_reg
 #(parameter integer WIDTH = 32)
 (
@@ -18,6 +20,7 @@ module gpio_reg
         end
 endmodule
 
+// Counter module can count from 0 to 2^(WIDTH)-1 and will be circular going back to 0 when max is hit
 module counter
 #(parameter integer WIDTH = 2)
 (
@@ -34,6 +37,8 @@ module counter
     end
 endmodule
 
+// This module takes 4 gpio registers and groups them so the counter can easily select which one is in use
+// This module needs to be able to change the number of gpio registers from 4 to whatever the desired number is through a variable
 module gpio_out
 #(
     parameter integer N = 4,
@@ -58,6 +63,7 @@ module gpio_out
     gpio_reg #(.WIDTH(WIDTH)) out2(.rstn(reset), .clk(clk), .en(en2), .datain(datain), .dataout(dataout));
     gpio_reg #(.WIDTH(WIDTH)) out3(.rstn(reset), .clk(clk), .en(en3), .datain(datain), .dataout(dataout));
 
+    //Prints changes to the variables being strobed
     always @(posedge clk)
     begin
         $strobe("GPIO-out0 Pins: %b, %b", en0, out0.dataout);
@@ -74,6 +80,8 @@ module gpio_out
     end
 endmodule
 
+// This module takes 4 gpio registers and groups them so the counter can easily select which one is in use
+// This module needs to be able to change the number of gpio registers from 4 to whatever the desired number is through a variable
 module gpio_in
 #(
     parameter integer N = 4,
@@ -98,6 +106,7 @@ module gpio_in
     gpio_reg #(.WIDTH(WIDTH)) in2(.rstn(reset), .clk(clk), .en(en2), .datain(datain), .dataout(dataout));
     gpio_reg #(.WIDTH(WIDTH)) in3(.rstn(reset), .clk(clk), .en(en3), .datain(datain), .dataout(dataout));
 
+    //Prints changes to the variables being strobed
     always @(posedge clk)
     begin
         $strobe("GPIO-in0 Pins: %b, %b", en0, in0.dataout);
@@ -114,6 +123,7 @@ module gpio_in
     end
 endmodule
 
+//Manages the counter, in and out groups and simulates gpio activity using an initial block
 module gpio
 #(parameter integer WIDTH = 32,
 parameter integer N = 4)
@@ -121,35 +131,47 @@ parameter integer N = 4)
     reg clk;
     wire [1:0] count;
     reg reset, select, wen, ren;
-    reg [1:0] addr;
+    //eg [1:0] addr;
     reg [WIDTH-1:0] dataIN;
     wire [WIDTH-1:0] dataOUT;
     wire [WIDTH-1:0] gpioData;
+
+    assign  gpioData = 32'bzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz;
+    assign  dataOUT = 32'bzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz;
 
     always begin
 	    #50 clk = ~clk;
     end
 
+    //Prints changes to the variables being strobed
     always @(negedge clk)
     begin
         $strobe("\nClock Edge\n");
+        $strobe("GPIODATA: %b", gpioData);
+        $strobe("DATAOUT: %b", dataOUT);
     end
 
+    //Connect modules with regs and wires
     counter #() counter1(.clk(clk), .count(count));
-    gpio_out #(.WIDTH(WIDTH), .N(N)) out(.rstn(reset), .clk(clk), .sel(select), .wen(wen), .addr(addr), .datain(dataIN), .dataout(gpioData));
-    gpio_in #(.WIDTH(WIDTH), .N(N)) in(.rstn(reset), .clk(clk), .sel(select), .wen(ren), .addr(addr), .datain(gpioData), .dataout(dataOUT));
+    gpio_out #(.WIDTH(WIDTH), .N(N)) out(.rstn(reset), .clk(clk), .sel(select), .wen(wen), .addr(count), .datain(dataIN), .dataout(gpioData));
+    gpio_in #(.WIDTH(WIDTH), .N(N)) in(.rstn(reset), .clk(clk), .sel(select), .wen(ren), .addr(count), .datain(gpioData), .dataout(dataOUT));
 
+    //GPIO simulation
     initial
         begin
+            dataIN = 32'bzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz;
 	        clk = 1'b0;
             reset = 1'b0;
-            select = 1'b1;
-            wen = 1'b1;
-            ren = 1'b1;
+            select = 1'b0;
+            wen = 1'b0;
+            ren = 1'b0;
 
             @(negedge clk);
             reset = 1'b1;
-            addr = 0;
+
+            @(negedge clk);
+            select = 1'b1;
+            wen = 1'b1;
             dataIN = 32'b11111111111111111111111111111111;
 
             @(negedge clk);
